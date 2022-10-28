8bc563d398429267c5d157d078397ffc0118a1a7
dhughen93 <dhughen93@gmail.com>
2022-10-27 19:00:01 -0500

    First commit

---
+++ src/main/java/com/tag_excel_database_spring/tag_excel_database_spring/TagExcelDatabaseSpringApplication.java
@@ -0,0 +1,13 @@
+package com.tag_excel_database_spring.tag_excel_database_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TagExcelDatabaseSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TagExcelDatabaseSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tag_excel_database_spring/tag_excel_database_spring/TagExcelDatabaseSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tag_excel_database_spring.tag_excel_database_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TagExcelDatabaseSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
